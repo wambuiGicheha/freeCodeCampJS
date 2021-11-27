82145ca5bc3e21aed8f7520a09a5836b0e60c57a
k3zG33 <gichehakezg@gmail.com>
2021-11-27 18:45:07 +0300

    more else and switch statements

--- if.js
+++ if.js
@@ -12,3 +12,36 @@
  }
 
  trueOrFlase(true);
+
+ function noIfBolean(a,b){
+return a===b;
+
+ }
+ console.log(noIfBolean(2,2));
+
+ function ifItsTrue(a,b){
+     return a!==b;
+ }
+ console.log(ifItsTrue(9,7));
+
+ function myFun() {
+    console.log("Hello");
+    return "World";
+    console.log("byebye")
+  }
+  myFun();
+
+
+  // Setup
+function abTest(a, b) {
+    // Only change code below this line
+   if(a <0 || b < 0){
+     return undefined;     
+   }
+     // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
+  
--- switchcase.js
+++ switchcase.js
@@ -111,3 +111,104 @@ function sequentialSizes(val){
 }
 sequentialSizes(1);
 console.log(sequentialSizes(1));
+
+
+function chainToSwitch(val) {
+  let answer = "";
+  
+
+  switch(val) {
+    case "bob":
+    answer = "Marley";
+    break;
+
+      case 42:
+      answer = "The Answer";
+      break;
+
+      case 1: 
+      answer ="There is no #1";
+      break;
+
+      case 99:
+      answer = "Missed me by this much!";
+      break;
+
+      case 7:
+      answer = "Ate Nine";
+      break;
+      
+        }
+
+
+  return answer;
+}
+
+console.log(chainToSwitch(7));
+
+var count = 0;
+
+function cc(card) {
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  var holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+
+  return count + " " + holdbet;
+
+}
+
+
+cc(2); cc('K'); cc(10); cc('K'); cc('A');
+console.log(cc(4))
+/*
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch(card){
+  case 2 : 
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+   answer = "Bet";
+   break;
+
+   case 7:
+   case 8:
+   case 9:
+   answer = "Hold";
+break;
+
+case 10:
+case J:
+case Q:
+case K:
+case A:
+
+}
+
+  return "Hold";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+*/
