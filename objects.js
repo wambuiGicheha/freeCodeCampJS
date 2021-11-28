c573dc92adbc836dc0eb5c427272b52ae66b0274
k3zG33 <gichehakezg@gmail.com>
2021-11-29 00:15:52 +0300

    objects introduction

---
+++ objects.js
@@ -0,0 +1,11 @@
+const myName = {
+    "firstName" : "Elijah",
+    "secondName" :"Mikaelson",
+    "age" : 45,
+    "species" : "vampire"
+
+}
+const actingName = myName.firstName;
+const actingAge = myName.age;
+
+console.log("This character's name is " + actingName + " who is " + actingAge + " years old");
