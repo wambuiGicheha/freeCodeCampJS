1c2e7be700f388aafbe05b87044bba2e6665ab85
k3zG33 <gichehakezg@gmail.com>
2021-11-30 18:09:25 +0300

    objects

--- objects.js
+++ objects.js
@@ -1,11 +1,54 @@
-const myName = {
+const myCharacter = {
     "firstName" : "Elijah",
     "secondName" :"Mikaelson",
     "age" : 45,
     "species" : "vampire"
 
 }
-const actingName = myName.firstName;
-const actingAge = myName.age;
+
+// adding an object
+myCharacter.brother= "Klaus";
+
+// add object to remove
+myCharacter.father = "Ansel";
+
+//delete object 
+delete myCharacter.father;
+
+
+const actingName = myCharacter.firstName;
+const actingAge = myCharacter.age;
+const actingbro = myCharacter.brother;
+
+console.log(actingName + " has a brother called " + actingbro + "." );
 
 console.log("This character's name is " + actingName + " who is " + actingAge + " years old");
+
+console.log(myCharacter["father"]);
+
+function phoneticLookup(val) {  
+    var result = "";
+    var lookup = {
+        "alpha" : "Adams",
+        "bravo" : "Boston",
+        "charlie" : "Chicago",
+        "delta" : "Denver",
+        "echo" :" Easy",
+        "foxtrot" : "Frank"
+     };
+
+           result = lookup[val];
+            return result;
+}
+console.log(phoneticLookup("charlie"));
+
+myCharacter.hasOwnProperty("father");
+
+
+function checkObj(obj, checkProp) {
+    if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+  }
