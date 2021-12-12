1fd54b59b0c7050940c82f18fe619c992f610e42
k3zG33 <gichehakezg@gmail.com>
2021-12-12 05:47:59 +0300

    modified objects

---
+++ WHILE.js
@@ -0,0 +1,17 @@
+// Setup
+const myArray = [];
+// Only change code below this line
+let i = 0;
+while(i <= 5) {
+myArray.push(i);
+i++;
+}
+console.log(myArray);
+secondArray = [];
+let a = 5;
+while(a > -1) {
+    secondArray.push(a);
+    a--;
+}
+console.log(secondArray);
+
--- objects.js
+++ objects.js
@@ -108,3 +108,25 @@ const someplace = {
 ];//second item[1] myPlants.trees[1]
 const secondTree = myPlants[1].list[1];
 console.log(secondTree);
+
+
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+console.log(recordCollection[2468].albumTitle);
