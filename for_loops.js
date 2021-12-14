a6fb2deeda445844d5f9832f06a4f14c40c9898b
k3zG33 <gichehakezg@gmail.com>
2021-12-14 23:04:17 +0300

    for loops

---
+++ for_loops.js
@@ -0,0 +1,32 @@
+const myArray = [];
+
+for (let i=1; i <=10; i++){
+
+myArray.push(i);
+}
+
+console.log("The numbers are " + myArray);
+
+// Setup
+const myArray2 = [];
+
+
+for (let a = 0; a <= 9; a += 3){
+  myArray2.push(a);
+}
+console.log('The numbers divisible by 3 are ' + myArray2 + ".");
+
+
+const myArray3 = [];
+for (let j = 1; j <= 9; j += 2){
+  myArray3.push(j);
+}
+console.log("The odd numbers are " + myArray3 + ".");
+
+const array4 = [];
+for ( let b = 9; b >= 1; b -=2 ){
+array4.push(b);
+}
+console.log(array4);
+
+
